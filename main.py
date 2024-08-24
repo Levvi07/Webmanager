066aac04fc71ed60255151c084abc5d0e5c94599
Balog Levente <balog.levi07@gmail.com>
2024-08-24 11:37:06 +0200

    Users may change their shit now

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 from flask import send_from_directory, request, make_response
-import handle_users
+import handle_users, csv, hashlib
 import data_reader as dr
 dr.init()
 def create_app():
@@ -132,7 +132,7 @@ def remove_user():
 
 @app.route("/admin/users.html")
 def users():
-    perm_code = handle_users.check_site_perm("/admin/remove_user.html", request.cookies.get("token"))
+    perm_code = handle_users.check_site_perm("/admin/users.html", request.cookies.get("token"))
     if perm_code == "401":
         print("not allowed")
         return "", {"Refresh": "0; url=/401.html"}
@@ -142,6 +142,78 @@ def users():
         users += f"<tr><td class='id_td'>{id}</td><td class='name_td'>{dr.users_data[i+1][1]}</td><td class='modify_td'><a href='/modifyuser/{id}'>Modify</a></td><td class='del_td'><button onclick='delete_user({id},\"{dr.users_data[i+1][1]}\")'>Delete User</button></td></tr>"
     return serve_html_website("/admin/users.html").replace("USERS", users)    
 
+#Handle user pages
+@app.route("/user/<path:p>", methods=["GET","POST"])
+def user_page(p):
+    alert = ""
+    perm_code = handle_users.check_site_perm("/user/"+p, request.cookies.get("token"))
+    if perm_code == "401":
+        print("not allowed")
+        return "", {"Refresh": "0; url=/401.html"}
+    id = int(p)
+    #update data if its possible
+    if request.method == "POST":
+        #get data type set by form variable
+        rtype = request.form["rtype"]
+        if rtype == "uname":
+            username = request.form["username"]
+            email = request.form["email"]
+            full_name = request.form["full_name"]
+            description = request.form["description"]
+            new_users_data = dr.users_data
+            if username != dr.users_data[id][1]:
+                #username changed
+                usernames = []
+                for i in range(len(dr.users_data)-1):
+                    usernames.append(dr.users_data[i+1][1])
+                if username not in usernames:
+                    new_users_data[id][1] = username
+                else:
+                    #insert alert message if the name is still in use
+                    alert += "Username already in use!"
+            if email != dr.users_data[id][2]:
+                new_users_data[id][2] = email
+            if full_name != dr.users_data[id][3]:
+                new_users_data[id][3] = full_name
+            if description != dr.users_data[id][4]:
+                new_users_data[id][4] = description
+            f = open("./data/users.csv", "w", encoding="UTF-8", newline='')
+            writer = csv.writer(f)
+            for row in new_users_data:
+                writer.writerow(row)
+            f.close()
+            alert = "Data Changed, Logging out"
+        elif rtype == "pwd":
+            new_hash_data = dr.hash_data
+            c_pass = request.form["current_pass"]
+            pass1 = request.form["password1"]
+            pass2 = request.form["password2"]
+            if hashlib.md5(bytes(c_pass, "UTF-8")).hexdigest() != dr.hash_data[id][1]:
+                alert = "Current Password is Invalid!"
+            else:
+                if pass1 != pass2:
+                    alert = "Passwords do not match!"
+                else:
+                    new_hash_data[id][1] = hashlib.md5(bytes(pass1, "UTF-8")).hexdigest()
+                    alert = "Data Changed, Logging out"
+            f = open("./data/pwd_hashes.csv", "w", encoding="UTF-8", newline='')
+            writer = csv.writer(f)
+            for row in new_hash_data:
+                writer.writerow(row)
+            f.close()              
+
+
+    username = dr.users_data[id][1]
+    email = dr.users_data[id][2]
+    full_name = dr.users_data[id][3]
+    description = dr.users_data[id][4]
+    ret = serve_html_website("/user/index.html").replace("#DESCRIPTION#", description).replace("#EMAIL#", email).replace("#USERNAME#", username).replace("#FULL_NAME#", full_name)
+    if alert != "":
+        ret += "<script>alert('"+ alert +"')</script>"
+    if alert == "Data Changed, Logging out":
+        return "Data Changed, Logging out!", {"Refresh":"2; url=/signout.html"}
+    else:
+        return ret
 
 #handle any other static site
 @app.route('/<path:p>')
