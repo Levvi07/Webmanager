2557cf214ab6b610fb2a1f2e42ca84b76af389a1
Balog Levente <balog.levi07@gmail.com>
2026-04-06 13:02:50 +0200

    Fixed plugin path loading. It was checking for files in the wrong templates folder

--- plugins/Template_plugin/__plugin_init__.py
+++ plugins/Template_plugin/__plugin_init__.py
@@ -5,23 +5,25 @@
 # This file is a standardised file for all plugins
 # This file MUST exist in the root folder of the plugin otherwise it wont work
 # Use the page decorator for defining your endpoints
-# The data in the PluginData class is not compulsory, but advisable, EXCEPT the name field, which must not be empty
+# The data in the PluginData class is not compulsory, but advisable, EXCEPT the name, and path field, which must not be empty
 
 # only edit onwards from the "EDIT FROM HERE" flag as the first part is just operative and changing it might break your plugin
 #---------------------------------------------------------------
 class PluginData():
     name = "TemplatePlugin"
     version = "1.0"
-    description = "This is a template plugin with a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery laaang description"
+    description = "This is a template plugin"
     path = "/plugins/Template_plugin/"
     
 
 
 import os
 def serve_html_website(route):
-    if not os.path.exists("./templates/" + route):
+    if not os.path.exists("." + PluginData().path + "templates/" + route):
+        print("first          ", "." + PluginData().path + "templates/" + route)
         return "", {"Refresh": "0; url=/404.html"}
-    if route[-1] != "/" and os.path.isdir("./templates/" + route):
+    if route[-1] != "/" and os.path.isdir("." + PluginData().path + "templates/" + route):
+        print("secpnd")
         return "", {"Refresh": "0; url=/404.html"}
     pl_path = PluginData().path
     if pl_path[-1] != "/":
@@ -70,13 +72,12 @@ def endpoint(name):
 # You may use the @endpoint(endpoint) decorator to define a webpage function
 # an asterisk does the same thing, as what <path:p> does in flask
 
-import json
 import data_reader as dr
 @endpoint("/")
 def index(request):
     return serve_html_website("index.html").replace("CONFIG", str(dr.site_config_data)).replace("REQUEST", str(request.form))
 
-@endpoint("/admin/")
+@endpoint("/second/")
 def adminpage():
     return serve_html_website("SecondSite.html")
 
